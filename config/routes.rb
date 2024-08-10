17165d95d2f4fa7453f1554a82917a83f9988acf
Nathan Trautenberg <Natester123@mac.com>
2024-08-09 18:31:49 -0600

    homepage pictures that lead to sections on me.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+  
+  get 'teacher', to: 'home#teacher'
+  get 'software_engineer', to: 'home#software_engineer'
+  get 'phylogeneticist', to: 'home#phylogeneticist'
+  get 'writer', to: 'home#writer'
 end
