0c7feaa0055599e90d25e662773fc9d2ebb0e930
Nathan Trautenberg <Natester123@mac.com>
2024-08-10 20:56:15 -0600

    designed teacher section of website, including adding paragraphs and media and animated background.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,43 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { tsParticles } from "tsparticles";
+import { loadSeaAnemonePreset } from "@tsparticles/preset-sea-anemone";
+import { loadStarsPreset } from "@tsparticles/preset-stars";
+
+(async () => {
+  await loadSeaAnemonePreset(tsParticles);
+
+  await tsParticles.load({
+    id: "tsparticles",
+    options: {
+      preset: "seaAnemone",
+    },
+  });
+})();
+
+(async () => {
+  await loadStarsPreset(tsParticles);
+
+  await tsParticles.load({
+    id: "tsparticles",
+    options: {
+      preset: "stars",
+    },
+  });
+})();
+
+document.addEventListener("DOMContentLoaded", () => {
+  tsParticles.load("tsparticles", {
+    particles: {
+      number: {
+        value: 50
+      },
+      size: {
+        value: 3
+      }
+      // ... more configuration options
+    }
+  });
+});
+
